8eefa6ea01d8266ce257978ca368257f32047591
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-01-09 00:21:46 +0100

    it work now

--- code/b.js
+++ code/b.js
@@ -3,9 +3,9 @@ console.log("b.js --> loaded");
 p_l = document.querySelector('.p_layer');
 
 function trn(e) {
-	pageX = e.clientX - window.innerWidth*0.5;
-	pageY = e.clientY - window.innerHeight*0.5;
-  p_l.style.transform = 'translateX(-' + (50 + pageX/200) + '%) translateY(-' + (50 + pageY/200) + '%)';
+  pageX = e.clientX - window.innerWidth*0.5;
+  pageY = e.clientY - window.innerHeight*0.5;
+  p_l.style.transform = "translateX(" + (-pageX/100) + "%) " + "translateY(" + (-pageY/100) + "%)";
 }
 
-p_l.addEventListener('mousemove', trn(e));
+document.querySelector("body").addEventListener('mousemove', trn);
