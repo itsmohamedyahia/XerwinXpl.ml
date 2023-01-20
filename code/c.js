6a58b83eba15ed3e0875f681e25f5e31e703c29e
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-01-20 11:56:03 +0100

    Update c.js

--- code/c.js
+++ code/c.js
@@ -1,9 +1,17 @@
 console.log("c.js --> loaded");
 
 const lp = document.createElement("div");
+const tx = document.createElement("p");
+const txl = document.createElement("p");
 lp.id = "loader-page";
+txl.id = "txl";
+tx.innerText = "welcome to XerwinXpl website!";
+txl.innerText = "its loading...";
+lp.appendChild(tx);
+lp.appendChild(txl);
 
 const b = document.querySelector("body");
+
 b.appendChild(lp);
 
 function timed_a() {
