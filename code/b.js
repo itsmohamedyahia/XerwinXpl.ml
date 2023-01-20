1f7c8ce0b9131214cdb8252f8410bccf69c00649
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-01-20 18:29:09 +0100

    movement direction change

--- code/b.js
+++ code/b.js
@@ -7,7 +7,7 @@ function trn(e) {
   pageX = e.clientX - window.innerWidth*0.5;
   pageY = e.clientY - window.innerHeight*0.5;
   p_l.style.transform = "translateX(" + (-pageX/100) + "%) " + "translateY(" + (-pageY/100) + "%)";
-  p_bg.style.backgroundPosition = "" + -pageX/150 + "%" + -pageY/150 + "%"
+  p_bg.style.backgroundPosition = "" + -pageX/150 + "%" + pageY/150 + "%"
 }
 
 document.querySelector("body").addEventListener('mousemove', trn);
