b265a5fa8ef3fe281529faebbaf3e1989645de56
XerwinXpl <85256998+E15dev@users.noreply.github.com>
2023-02-10 11:05:03 +0100

    fixed html, fixed issue #5
    when "d" is loaded after "c" its broken

--- code/d.js
+++ code/d.js
@@ -1,6 +1,6 @@
 console.log("d.js --> loaded");
 
-d_data = `
+d_data_b = `
 		<div id="bottom">
 			<div id="tags">
 				<div class="mtag" id="e"><p>electronica</p></div>
@@ -10,28 +10,34 @@ d_data = `
 				<div class="mtag" id="h"><p>house</p></div>
 			</div>
 			<div id="releases">
-			<div class="release-icon" id="ri0"><a href="https://xerwinxpl.bandcamp.com/track/i-think/"><img src="https://f4.bcbits.com/img/a3340981925_2.jpg"></a></div>
-				<div class="release-icon" id="ri1"><a href="https://xerwinxpl.bandcamp.com/album/2-weeks"><img src="https://f4.bcbits.com/img/a1300888331_2.jpg"></a></div>
-				<div class="release-icon" id="ri2"><a href="https://xerwinxpl.bandcamp.com/track/not-acid"><img src="https://f4.bcbits.com/img/a0933733175_2.jpg"></a></div>
-				<div class="release-icon" id="ri3"><a href="https://xerwinxpl.bandcamp.com/track/things-i-will-never-have"><img src="https://f4.bcbits.com/img/a3412206100_2.jpg"></a></div>
-				<div class="release-icon" id="ri4"><a href="https://xerwinxpl.bandcamp.com/album/album03"><img src="https://f4.bcbits.com/img/a3216786508_2.jpg"></a></div>
-			</div>
-			<div id="dcinvite">
-				<a href="https://www.discord.com/invite/rWNxcGuK9R"><p>join discord</p></a>
+				<div class="release-icon" id="ri0"><a href="https://xerwinxpl.bandcamp.com/track/i-think/"><img src="https://f4.bcbits.com/img/a3340981925_2.jpg"></a></div>
+					<div class="release-icon" id="ri1"><a href="https://xerwinxpl.bandcamp.com/album/2-weeks"><img src="https://f4.bcbits.com/img/a1300888331_2.jpg"></a></div>
+					<div class="release-icon" id="ri2"><a href="https://xerwinxpl.bandcamp.com/track/not-acid"><img src="https://f4.bcbits.com/img/a0933733175_2.jpg"></a></div>
+					<div class="release-icon" id="ri3"><a href="https://xerwinxpl.bandcamp.com/track/things-i-will-never-have"><img src="https://f4.bcbits.com/img/a3412206100_2.jpg"></a></div>
+					<div class="release-icon" id="ri4"><a href="https://xerwinxpl.bandcamp.com/album/album03"><img src="https://f4.bcbits.com/img/a3216786508_2.jpg"></a></div>
+				</div>
 			</div>
 		</div>
+`
+
+d_data_c = `
 		<div class="ab" id="ab-l">
 			<p>other articles</p>
 			<a href="https://www.xerwinxpl.ml/art/2023/sanctuary.html"><p>Sanctuary collab</p></>
 		</div>
+`
+
+d_data_d = `
 		<div class="ab" id="ab-r">
 			<p>socials</p>
 			<a href="https://twitter.com/XerwinXpl"><i class="fa-brands fa-twitter center"></i></a>
-			<a rel="me" href="https://mastodon.social/@XerwinXpl"><i class="fa-brands fa-mastodon center"></i></a>
+			<a href="https://mastodon.social/@XerwinXpl" rel="me"><i class="fa-brands fa-mastodon center"></i></a>
 			<a href="https://xerwinxpl.bandcamp.com"><i class="fa-brands fa-bandcamp"></i></a>
 			<a href="https://www.youtube.com/@xerwinxpl"><i class="fa-brands fa-youtube"></i></a>
 			<a href="https://github.com/e15dev"><i class="fa-brands fa-github"></i></a>
 		</div>
 `
 
-document.write(d_data)
+document.write(d_data_b)
+document.write(d_data_d)
+document.write(d_data_c)
