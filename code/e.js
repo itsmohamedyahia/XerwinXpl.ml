a304860deb1cd9ea6cbf6a0a99b7baff5237ef43
itsmohamedyahia <justmyahia@gmail.com>
2023-03-31 21:19:32 +0200

    add random color functionality

--- code/e.js
+++ code/e.js
@@ -4,8 +4,18 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+function getRandomColor() {
+  const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+  return randomColor;
+}
+
 dr = document.querySelectorAll(".dr");
+const randomBgEls = document.querySelectorAll('.random-bg');
 
 for (let i = 0; i < dr.length; i++) {
     dr[i].style.animationDelay = "" + (getRandomInt(250)*4) + "ms";
 }
+
+randomBgEls.forEach((el) => {
+  el.style.setProperty('--random-bg-color', getRandomColor());
+});
